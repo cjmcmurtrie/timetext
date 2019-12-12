d9967f97f3b04e7779c5182726bba2bd88ad7594
Conan McMurtrie <cj.mcmurtrie@gmail.com>
2019-12-12 11:23:56 +0000

    instantiate db/tables, insert relations

--- tests/test_tt.py
+++ tests/test_tt.py
@@ -1,10 +1,26 @@
-from timetext.tt import Timetext
+from timetext.tt import Timetext, FETCH_QUERIES
 
 
-def test_initialize_project():
+def test_initialize_new_project():
+    project_name = 'test_project'
+    Timetext(project_name)
+
+
+def test_insert_relation():
+    '''
+    Insert relation into
+    '''
     project_name = 'test_project'
     tt = Timetext(project_name)
+    relation = [
+        '2003-01-01 00:24:52.158000+00:00',
+        'this is a concept', 'this is a related concept',
+        'co_occurrence', 1
+    ]
+    tt.insert_relation(relation)
+    assert 'this is a concept' in tt.fetch('this is a concept')[0]
 
 
 if __name__ == '__main__':
-    test_initialize_project()
+    test_initialize_new_project()
+    test_insert_relation()
--- timetext/tt.py
+++ timetext/tt.py
@@ -1,6 +1,45 @@
 import sqlite3 as sql
 
 
+CREATE_TABLE_QUERIES = {
+    'sql_create_relations_table': """ 
+            CREATE TABLE IF NOT EXISTS relation (
+            id integer PRIMARY KEY,
+            time timestamp,
+            concept_1 text NOT NULL,
+            concept_2 text NOT NULL,
+            relation text NOT NULL,
+            relation_value text
+        );
+    """,
+    'sql_create_concepts_table': """
+            CREATE TABLE IF NOT EXISTS concept (
+            id integer PRIMARE KEY,
+            time timestamp,
+            concept text NOT NULL,
+            property text,
+            property_type text
+        );
+    """
+
+}
+
+
+INSERT_QUERIES = {
+    'sql_insert_relation': """
+        INSERT INTO relation(time, concept_1, concept_2, relation, relation_value)
+        VALUES(?,?,?,?,?)
+    """
+}
+
+
+FETCH_QUERIES = {
+    'sql_concept_1_relations': """
+        SELECT * FROM relation WHERE concept_1=?;
+    """
+}
+
+
 class Timetext(object):
 
     def __init__(self, project_name):
@@ -10,7 +49,20 @@ class Timetext(object):
         self._init()
 
     def _init(self):
-        pass
+        cursor = self.db.cursor()
+        cursor.execute(CREATE_TABLE_QUERIES['sql_create_relations_table'])
+        self.db.commit()
+
+    def fetch(self, concept_1, time_window=None):
+        cursor = self.db.cursor()
+        query = FETCH_QUERIES['sql_concept_1_relations']
+        rows = cursor.execute(query, (concept_1, )).fetchall()
+        return rows
+
+    def insert_relation(self, row):
+        cursor = self.db.cursor()
+        cursor.execute(INSERT_QUERIES['sql_insert_relation'], row)
+        self.db.commit()
 
     def populate(self, rows):
         pass
