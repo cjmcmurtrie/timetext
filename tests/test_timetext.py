f47af6a20e380af7aeb8954b7b7853cf50f0da26
Conan McMurtrie <cj.mcmurtrie@gmail.com>
2019-12-19 18:17:50 +0000

    update tests

--- tests/test_timetext.py
+++ tests/test_timetext.py
@@ -44,6 +44,17 @@ def test_timetext_populate_1000_spacy(test_file='data/1000.csv'):
     assert end - start <= 0.6
 
 
+def test_timetext_populate_1000_stopwords(test_file='data/1000.csv'):
+    times, texts = zip(*get_1000_test_documents())
+    tags = [] * len(times)
+    tt = timetext(PROJECT_NAME)
+    start = time.time()
+    tt.populate(times, texts, tags=tags, mode='stopwords')
+    end = time.time()
+    print('test: computed 1000 short documents in', end - start)
+    assert end - start <= 0.6
+
+
 def test_hops():
     tt = timetext(PROJECT_NAME)
     timestamped_texts = [
